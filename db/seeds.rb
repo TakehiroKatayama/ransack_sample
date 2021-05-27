1a6e62caece72cfc7076d923f107a8ea082ffd98
TakehiroKatayama <t.katayama.info@gmail.com>
2021-05-27 18:42:53 +0900

    [add]faker/seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+require "faker"
+Faker::Config.locale = :ja
+
+# 1000件の初期データを投入
+1000.times do
+  User.create!(name: Faker::Name.name, age: rand(0..100))
+end
+
+puts "初期データの投入に成功しました！"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
